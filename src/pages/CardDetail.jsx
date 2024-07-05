e1cbdcec408098d793973c3dd21a0aabb3e65efe
Vladislav Lebedkin <vladislav_lebedkin@mail.ru>
2024-07-05 19:36:17 +0300

    добавил стор для работы с продуктами

--- src/pages/CardDetail.jsx
+++ src/pages/CardDetail.jsx
@@ -1,46 +1,33 @@
 import { useParams } from "react-router-dom";
-import { initialProducts } from "../../data.js";
+import useProductsStore from "../store/useProductsStore.js";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 const CardDetail = () => {
-  const [isFavorite, setFavorite] = useState(false); // Стейт для того, чтобы пометить товар сохраненным или нет.
-
   const { id } = useParams();
 
+  // Стор для работы с продуктами
+  const { products, setFavorite } = useProductsStore();
+
   // Находим карточку по id.
-  const product = initialProducts?.find((product) => product?.id === id);
-
-  useEffect(() => {
-    // Получаем данные из localStorage
-    const storedFavoriteProducts = localStorage.getItem("favorite");
-
-    // Если данные есть в localStorage, устанавливаем начальное состояние продуктов
-    if (storedFavoriteProducts) {
-      const favoriteProducts = JSON.parse(storedFavoriteProducts);
-
-      const isProductInStorage = favoriteProducts.includes(id);
-
-      // Обновляем стейт сохраненным товаром.
-      setFavorite(isProductInStorage);
-    }
-  }, [id]);
+  const product = products?.find((product) => product?.id === id);
 
   return (
     <section className="card-details">
       <div className="container mx-auto p-4">
         <Link
           to="/cards"
-          className=" text-gray-600 hover:text-gray-900 mb-8 inline-flex"
+          className=" text-indigo-500 hover:text-indigo-600 border-b-2 border-b-indigo-500 mb-8 inline-flex"
         >
           Вернуться назад
         </Link>
         <div className="max-w-md rounded shadow-lg relative">
-          <img className="w-full" src={product?.imgSrc} alt={product?.title} />
+          <img className="w-full " src={product?.imgSrc} alt={product?.title} />
+
           <button
             className={`absolute top-0 left-0 m-2 p-2 rounded-full ${
-              isFavorite ? "text-red-500" : "text-white"
+              product?.isFavorite ? "text-indigo-500" : "text-white"
             }`}
+            onClick={() => setFavorite(id)}
           >
             <svg
               className="w-6 h-6 fill-current"
@@ -61,7 +48,7 @@ const CardDetail = () => {
               </div>
             )}
             <div className="text-lg font-bold mb-2">{product?.price}$</div>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            <button className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded">
               Add to Cart
             </button>
           </div>
